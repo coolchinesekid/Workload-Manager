e298ab719fffddb0f131de38a3e18e19ab37c934
Ethan Ng <ethan_ng_224@yahoo.com>
2020-05-12 19:19:07 +0800

    Initial Commit

---
+++ WorkloadManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkloadManager
+//
+//  Created by Ethan Ng on 5/12/20.
+//  Copyright © 2020 Ethan Ng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
